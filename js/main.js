b091502cd2a3b5df16535ec091d7b9e36f2dec15
tuum2 <68572839+tuum2@users.noreply.github.com>
2020-10-06 13:50:20 +0900

    202010060150

--- js/main.js
+++ js/main.js
@@ -1,23 +1,75 @@
-/*window.addEventListener("DOMContentLoaded",function(){
+/*
+window.addEventListener('scroll',function(){
     
-   console.log("?");
+   const parallax = document.querySelector('#video');
+    let scrollPosition = parallax.offsetTop;
+    console.log(scrollPosition);
     
-});*/
+    parallax.style.transform="translateY(50px)"; 
+    
+});
+*/
 
 
 
-$(document).ready(function(){
+
+$(document).ready(function () {
+
+    $('.banner').parallax('70%', 0.3);
+
+    /*$("#wrap").smoothWheel();
+    $("#wrap").scrollStopped(function(){
+                console.log("스크롤끝확인!");
+	// 부드러운 스크롤 세로 스크롤값에 현재 스크롤위치값 넣기!
+	// 부호는 반대임!
+                currentY = -$(this).scrollTop();
+            });
+*/
+
+    $('.menu_toggle').on('click', function () {
+
+       
+        $('.gnbarea').fadeIn();
+
+    });
     
-    
-	$('.menu_toggle').on('click',function(){
+    $('.gnbclose').click(function(){
+       
+        $('.gnbarea').fadeOut();
         
-        console.log("?");
-        
-		$('.menu_toggle').toggleClass('active');
-        
-	});
+    });
+    
+    
+    
+/* 고 탑 버튼 */
+    $(window).scroll(function () {
 
-    
-  
-    
+        $('.gotop').click(function () {
+
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+
+            return false;
+
+        }); // click /////////////////
+
+    }); // scroll ////////////
+
+
 }); //// jQB ///////////////
+
+
+
+
+
+
+
+$.fn.scrollStopped = function (callback) { //제이쿼리플러그인 만드는방식.
+    var _this = this,
+        $this = $(_this);
+    $this.scroll(function (event) {
+        clearTimeout($this.data('scrollTimeout'));
+        $this.data('scrollTimeout', setTimeout(callback.bind(_this), 250, event));
+    });
+}; //////////// 스크롤멈춤 이벤트 함수 ////////////////////
